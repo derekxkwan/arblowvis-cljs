091e7bd691bc93c12e619bd39a3d6d55e502234a
Derek Kwan <derek.x.kwan@gmail.com>
2019-02-07 03:48:26 -0800

    build with 4 models

---
+++ docs/lib/textureanim.js
@@ -0,0 +1,49 @@
+//courtesy of stemkoski
+
+function TextureAnimator(texture, tilesHoriz, tilesVert, numTiles, tileDispDuration) 
+{	
+	// note: texture passed by reference, will be updated by the update function.
+		
+	this.tilesHorizontal = tilesHoriz;
+	this.tilesVertical = tilesVert;
+	// how many images does this spritesheet contain?
+	//  usually equals tilesHoriz * tilesVert, but not necessarily,
+	//  if there at blank tiles at the bottom of the spritesheet. 
+	this.numberOfTiles = numTiles;
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping; 
+	texture.repeat.set( 1 / this.tilesHorizontal, 1 / this.tilesVertical );
+	// how long should each image be displayed?
+	this.tileDisplayDuration = tileDispDuration;
+	// how long has the current image been displayed?
+	this.currentDisplayTime = 0;
+	// which image is currently being displayed?
+	this.currentTile = 0;
+		
+	this.update = function( milliSec )
+	{
+		this.currentDisplayTime += milliSec;
+		while (this.currentDisplayTime > this.tileDisplayDuration)
+		{
+			this.currentDisplayTime -= this.tileDisplayDuration;
+			this.currentTile++;
+			if (this.currentTile == this.numberOfTiles)
+				this.currentTile = 0;
+			var currentColumn = this.currentTile % this.tilesHorizontal;
+			texture.offset.x = currentColumn / this.tilesHorizontal;
+			var currentRow = Math.floor( this.currentTile / this.tilesHorizontal );
+			texture.offset.y = currentRow / this.tilesVertical;
+		}
+	};
+}
+
+/* EXAMPLE USE
+ *
+ *
+ var runnerTexture = new THREE.ImageUtils.loadTexture( 'images/run.png' );
+	annie = new TextureAnimator( runnerTexture, 10, 1, 10, 75 ); // texture, #horiz, #vert, #total, duration.
+	var runnerMaterial = new THREE.MeshBasicMaterial( { map: runnerTexture, side:THREE.DoubleSide } );
+	var runnerGeometry = new THREE.PlaneGeometry(50, 50, 1, 1);
+	var runner = new THREE.Mesh(runnerGeometry, runnerMaterial);
+	runner.position.set(-100,25,0);
+	scene.add(runner);
+ */
